c4f21c8d5b2697597102469f685b596ba2ac88c7
shoonia <zaytsev126@gmail.com>
2018-09-25 17:48:08 +0300

    Formate datetime

--- src/components/pages/helpers/Time.jsx
+++ src/components/pages/helpers/Time.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const { languages } = navigator;
+const options = {
+  year: 'numeric',
+  month: 'short',
+  weekday: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  // second: 'numeric',
+};
+
 const Time = ({ date }) => (
   <time dateTime={date} className="has-text-grey-dark is-size-7">
-    {new Date(date).toLocaleString()}
+    {new Date(date).toLocaleString(languages, options)}
   </time>
 );
 
