017ce0a8911d8f983d0bb9a6b9f65e9e9d29c2ad
Alexander Zaytsev <zaytsev126@gmail.com>
2018-11-29 00:09:32 +0200

    Refresh node modules

--- src/utils/devToolsExtension.js
+++ src/utils/devToolsExtension.js
@@ -1,7 +1,8 @@
 export default () => {
   if (process.env.NODE_ENV === 'development') {
     if ('devToolsExtension' in window) {
-      return window.devToolsExtension();
+      // eslint-disable-next-line no-underscore-dangle
+      return window.__REDUX_DEVTOOLS_EXTENSION__();
     }
   }
   return f => f;
