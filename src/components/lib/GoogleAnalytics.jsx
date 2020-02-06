f527624cc976ccab39438ed49eab931fd13824bd
shoonia <zaytsev126@gmail.com>
2020-02-06 22:29:17 +0200

    Move GA to hooks

--- src/components/lib/GoogleAnalytics.jsx
+++ src/components/lib/GoogleAnalytics.jsx
@@ -1,51 +1,28 @@
-import React from 'react';
-import ReactGA from 'react-ga';
-import { withRouter } from 'react-router-dom';
+import { useEffect } from 'react';
+import ga from 'react-ga';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class GoogleAnalytics extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pathname: '',
-    };
-  }
-
-  componentDidMount() {
-    const { trackingId, history, location } = this.props;
-
-    if (process.env.NODE_ENV === 'development') {
-      return;
-    }
-
-    ReactGA.initialize(trackingId);
-    this.historyListener(location);
-    history.listen(this.historyListener);
-  }
-
-  historyListener = ({ pathname }) => {
-    const { state } = this;
-
-    if (state.pathname !== pathname) {
-      ReactGA.pageview(pathname);
-      this.setState({ pathname });
-    }
-  };
-
-  render() {
+function GoogleAnalytics({ trackingId }) {
+  if (process.env.NODE_ENV === 'development') {
     return null;
   }
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    ga.initialize(trackingId);
+  }, []);
+
+  useEffect(() => {
+    ga.pageview(pathname);
+  }, [pathname]);
+
+  return null;
 }
 
 GoogleAnalytics.propTypes = {
   trackingId: PropTypes.string.isRequired,
-  history: PropTypes.shape({
-    listen: PropTypes.func.isRequired,
-  }).isRequired,
-  location: PropTypes.shape({
-    pathname: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
-export default withRouter(GoogleAnalytics);
+export default GoogleAnalytics;
