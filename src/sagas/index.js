bb7e295c84d029684905abf4313aeaaba9547244
shoonia <alexanderz@wix.com>
2023-06-13 22:40:35 +0300

    Clean project

--- src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'redux-saga/effects';
-
-import listSaga from './list';
-import memeSaga from './meme';
-
-export default function* rootSaga() {
-  yield spawn(listSaga);
-  yield spawn(memeSaga);
-}
--- src/utils/devToolsExtension.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  if (process.env.NODE_ENV === 'development') {
-    if ('__REDUX_DEVTOOLS_EXTENSION__' in window) {
-      // eslint-disable-next-line no-underscore-dangle
-      return window.__REDUX_DEVTOOLS_EXTENSION__();
-    }
-  }
-  return (f) => f;
-};
